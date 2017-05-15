3d3b7ec95ecf46e53e8d5a265ad791be4ca00dcf
alisaleemh <ali.s1995@gmail.com>
2017-05-14 20:32:42 -0400

    added sqllite db

--- project.py
+++ project.py
@@ -1,11 +1,29 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
 @app.route('/')
-@app.route('/hello')
+@app.route('/restaurants')
 def helloWorld():
-    return "Hello World"
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for item in items:
+        output += item.name
+        output += '<br>'
+    return output
+
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)
